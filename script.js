87498a9928bc8c1122aac2d3c0388098d9eeecdf
mchlol <m.andreabr@gmail.com>
2021-05-29 16:57:11 +1000

    reordered logical tests

--- script.js
+++ script.js
@@ -35,50 +35,67 @@ let computerWin = 0;
 let playerChoice;
 let computerChoice;
 let roundResult;
+let invalidChoice = "Invalid choice!";
+
 
 function playRound(playerChoice, computerChoice) { 
-    playerChoice = playerChoice.toUpperCase(); 
-    computerChoice = computerPlay(); 
-    console.log(`You played ${ playerChoice }!`); 
-    console.log(`Computer played ${ computerChoice }!`);
-    if (playerChoice == computerChoice) { 
-      roundResult = "It's a tie!";
-      return roundResult;
-    } else if (
-              (playerChoice == "ROCK" && computerChoice == "SCISSORS") ||
-              (playerChoice == "PAPER" && computerChoice == "ROCK") ||
-              (playerChoice == "SCISSORS" && computerChoice == "PAPER")
-              ) {
-      playerWin++;
-      roundResult = "You WON!";
-      return roundResult;
-    } else if (
-              (playerChoice == "SCISSORS" && computerChoice == "ROCK") ||
-              (playerChoice == "ROCK" && computerChoice == "PAPER") ||
-              (playerChoice == "PAPER" && computerChoice == "SCISSORS")
-              ) {
-      computerWin++;
-      roundResult = "You LOST!";
-      return roundResult;
-    } else {
-      roundResult = "Invalid player choice";
-      return roundResult;
-    }
+  playerChoice = playerChoice.toUpperCase(); 
+  computerChoice = computerPlay(); 
+  console.log(`You played ${ playerChoice }!`); 
+  console.log(`Computer played ${ computerChoice }!`);
+
+  if (
+     (playerChoice !== "ROCK") &&
+     (playerChoice !== "PAPER") &&
+     (playerChoice !== "SCISSORS")
+     ) {
+    alert("Invalid Choice!");
+    roundResult = invalidChoice;
+    return invalidChoice;
+
+  } else if (
+            (playerChoice == "ROCK" && computerChoice == "SCISSORS") ||
+            (playerChoice == "PAPER" && computerChoice == "ROCK") ||
+            (playerChoice == "SCISSORS" && computerChoice == "PAPER")
+            ) {
+    playerWin++;
+    roundResult = "You WON!";
+    return roundResult;
+
+  } else if (
+            (playerChoice == "SCISSORS" && computerChoice == "ROCK") ||
+            (playerChoice == "ROCK" && computerChoice == "PAPER") ||
+            (playerChoice == "PAPER" && computerChoice == "SCISSORS")
+            ) {
+    computerWin++;
+    roundResult = "You LOST!";
+    return roundResult;
+
+  } else {
+    roundResult = "It's a tie!";
+    return roundResult;
+  }
 };
 
 // check number of player or computer wins and return score 
 // if either is over 5 alert user to start again
 
 function checkScore(playerWin, computerWin) {
-  if (playerWin === 5) {
-    return alert(`You won the game! Take that, Computer!`);
-  } else if (computerWin === 5) {
-    return alert(`Computer won the game! The machines are taking over!`);
-  } else if ((playerWin < 5) && (computerWin < 5)) {
-    return `Keep playing to win best of 5!`;
+  if (roundResult === invalidChoice) {
+    return "Invalid choice, try again";
+
   } else if ((playerWin > 5) || (computerWin > 5)) {
     let refreshAlert = alert("Stop, he's already dead! Refresh to play again.");
     return refreshAlert;
+
+  } else if (playerWin === 5) {
+    return alert(`You won the game! Take that, Computer!`);
+
+  } else if (computerWin === 5) {
+    return alert(`Computer won the game! The machines are taking over!`);
+
+  } else {
+    return `Keep playing, first to 5 wins!`;
   }
 };
 
